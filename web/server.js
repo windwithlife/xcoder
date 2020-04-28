e173b10be8c733f4cf1f1a683f379572cc3195c3
windwithlife@hotmail.com <windwithlife@hotmail.com>
2020-04-28 11:04:49 +0800

    update CORS

--- web/server.js
+++ web/server.js
@@ -29,8 +29,7 @@ app.prepare()
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "X-Requested-With");
       res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-      res.header("X-Powered-By",' 3.2.1')
-      res.header("Content-Type", "application/json;charset=utf-8");
+      res.header("X-Powered-By",' 3.2.1');
       next();
   });
   
