4ff0cf9bf33fd906fa7ab2da2381b8eea184d9b2
windwithlife@hotmail.com <windwithlife@hotmail.com>
2020-04-28 10:53:25 +0800

    add CORS

--- web/server.js
+++ web/server.js
@@ -25,6 +25,15 @@ app.prepare()
     // server.get('/a', (req, res) => {
     //   return app.render(req, res, '/b', req.query)
     // })
+    server.all('*', function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+      res.header("X-Powered-By",' 3.2.1')
+      res.header("Content-Type", "application/json;charset=utf-8");
+      next();
+  });
+  
 
     server.get('/generateCode', (req, res) => {
       console.log(req.query);
