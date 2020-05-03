bb1cfbba8334b910697fcf2226601b721fe8bb93
windwithlife@hotmail.com <windwithlife@hotmail.com>
2020-05-04 00:22:14 +0800

    improve website auto generating code

--- web/pages/config.js
+++ web/pages/config.js
@@ -1,4 +1,4 @@
-var DEV = {SOA_GATE:'http://localhost:38888',WEB_GATE:'http://localhost:8080'}
+var DEV = {SOA_GATE:'http://localhost:38888',WEB_GATE:'http://gateway.koudaibook.com:8080'}
 var UAT = {SOA_GATE:'https://service.zhangyongqiao.com:8080'}
 var PRODUCTION = {SOA_GATE:'http://gateway.zhangyongqiao.com/coder/',WEB_GATE:'http://gateway.zhangyongqiao.com/coder/'}
 var DOCKER = {SOA_GATE:'http://dockerhost:8080'}
