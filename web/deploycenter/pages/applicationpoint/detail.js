e700903e6e637d7a22a1a6d043e1d1bfad65c08a
windwithlife@hotmail.com <windwithlife@hotmail.com>
2020-11-09 19:47:33 +0800

    refresh Network

--- web/deploycenter/pages/applicationpoint/detail.js
+++ web/deploycenter/pages/applicationpoint/detail.js
@@ -18,8 +18,8 @@ const { TextArea } = Input;
 import router from 'next/router';
 import { inject, observer } from 'mobx-react';
 import EditTable from '../common/components/EditableTable';
-import NetworkHelper from '../../store/network';
-//import AddorEditPage from './AddorEditColumn';
+
+
 
 
 @inject('applicationPointStore') @inject('applicationTypesStore')
